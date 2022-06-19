c321e2a7fb1a0efc7172dd87b5dc23bddcc28fad
Hitesh <work@ihitesh.com>
2022-06-19 19:04:25 -0400

    printing objects

---
+++ user.js
@@ -0,0 +1,12 @@
+const task = {
+    description:'',
+    endDate:1,
+    hourlyRate:1,
+    id:0,
+    name:'',
+    startDate:1,
+    status:'',
+    totalHours:0
+  };
+
+  var myTasksArr = new Array(task);
